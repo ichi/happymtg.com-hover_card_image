b59a4a865f9732ceac4e63bef9d6031e8f63ff74
ichi <ichi.ttht.1@gmail.com>
2011-04-12 05:08:53 +0900

    jQuery.data使用に。同カードが複数回出てくる場合の対応。

--- happymtg.com-hover_card_image.user.js
+++ happymtg.com-hover_card_image.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           happymtg.com hover_card_image
 // @namespace      happymtg_com_hover_card_image
-// @version        0.2.3
+// @version        0.2.5
 // @include        http://www.happymtg.com/*
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js
 // @require        https://github.com/ichi/greasemonkey_console/raw/master/console.js
@@ -11,77 +11,94 @@
 var after_loaded_jquery = function($){ // $ = jQuery
 if(!$) return;
 
+//overwrite jQuery.data
+$.__data = $.data;
+$.data = function(elem, name, data){
+    elem = elem.wrappedJSObject || elem;
+    return $.__data(elem, name, data);
+};
+
+
+//main
 var $body = $('body');
 var $card_links = $('span.cardLink > a');
-var datas = {};
+var cached = {};
 
-var get_data = function($link, name){
-    var href = $link.attr('href');
-    var data = datas[href];
-    if(!data) return false;
-    if(name){
-        return data[name] || false;
-    }
-    return data;
-}
-var set_data = function($link, hash){
-    var href = $link.attr('href');
-    datas[href] = datas[href] || {};
-    $.extend(datas[href], hash);
-}
-
-var show_card_img = function($link){
-    var data = get_data($link);
-    if(!data) return;
+var show_popup = function($link){
+    var data = $link.data();
     if(!data.hover) return;
-    var $img = data.card_img;
-    var $text = data.card_text;
-    $img.show();
-    $text.show();
-};
-
-var hide_card_img = function($link){
-    var data = get_data($link);
-    if(!data) return;
-    var $img = data.card_img;
-    var $text = data.card_text;
-    if($img && $text){
-        $img.hide();
-        $text.hide();
+    var $popup = data.card_popup;
+    if($popup){
+        $popup.show();
     }
 };
 
-var on_success = function(data, status, xhr){
-    var $link = $(this);
+var hide_popup = function($link){
+    var data = $link.data();
+    var $popup = data.card_popup;
+    if($popup){
+        $popup.hide();
+    }
+};
+
+var set_text_pos = function($text, $img){
+    $img
+        .load(function(ev){
+            $text.css('left', this.naturalWidth + 10);
+        });
+};
+
+var set_popup = function($link, $img, $text, clone){
+    clone = clone || false;
+    
+    var href = $link.attr('href');
     var offset = $link.offset();
-    var $data = $(data);
     var width = $link.width(),
-    height = $link.height();
-    var $img = $data.find('img.jpnItem')
+        height = $link.height();
+    
+    if(clone){
+        $img = $img.clone();
+        $text = $text.clone();
+    }
+    
+    var $popup = $('<div />')
         .css({
             position: 'absolute'
             , display: 'none'
             , top: offset.top + height + 5
             , left: offset.left + width / 2
         })
+        .append($img)
+        .append($text);
+    
+    set_text_pos($text, $img);
+    
+    $body
+        .append($popup);
+    
+    $link.data('card_popup', $popup);
+};
+
+//ajax on_success
+var on_success = function(data, status, xhr){
+    var $link = $(this);
+    var $data = $(data);
+    var href = $link.attr('href');
+    
     var $card_detail = $data.find('#carddetail');
     var text_ja = $card_detail.find('tr.jpnItem:eq(1) > td').text().replace(/\n/g, '<br />');
     var text_en = $card_detail.find('tr.engItem:eq(1) > td').text().replace(/\n/g, '<br />');
     var $text = $('<div />')
         .css({
             position: 'absolute'
-            , display: 'none'
+            , top: 0
+            , left: 0
             , width: 200
-            , top: offset.top + height + 5
-            , left: offset.left + width / 2
             , padding: '5px'
             , background: '#ffffee'
             , border: '1px solid #000000'
             , color: '#000000'
         });
-    $img.load(function(ev){
-        $text.css('left', offset.left + width / 2 + 10 + this.naturalWidth);
-    });
     var $hr = $('<hr />')
         .css({
             height: 0
@@ -92,45 +109,53 @@ var on_success = function(data, status, xhr){
     $text
         .append('<div>' + text_ja + '</div>')
         .append($hr)
-        .append('<div>' + text_en + '</div>')
-    $body
-        .append($img)
-        .append($text);
+        .append('<div>' + text_en + '</div>');
     
-    set_data($link, {
-        card_img: $img
-        , card_text: $text
-    });
-    show_card_img($link);
+    var $img = $data.find('img.jpnItem')
+        .css({
+            position: 'absolute'
+            , top: 0
+            , left: 0
+        });
+    
+    cached[href] = {img:$img, text:$text};
+    
+    set_popup($link, $img, $text);
+    show_popup($link);
 };
 
+//attach
 $card_links
     .live('mouseenter', function(ev){
         var $link = $(this);
-        set_data($link, {hover: true});
-        var data = get_data($link);
-        if(data.card_img){
-            show_card_img($link);
+        $link.data('hover', true);
+        var data = $link.data();
+        if(data.card_popup){
+            show_popup($link);
         }else{
             var href = $link.attr('href');
-            $.ajax({
-                url: href
-                , context: $link[0]
-                , type: 'GET'
-                , success: on_success
-            });
+            if(cached[href]){
+                set_popup($link, cached[href].img, cached[href].text, true);
+                show_popup($link);
+            }else{
+                $.ajax({
+                    url: href
+                    , context: $link[0]
+                    , type: 'GET'
+                    , success: on_success
+                });
+            }
         }
-        set_data($link, {hover: true});
     })
     .live('mouseleave', function(ev){
         var $link = $(this);
-        set_data($link, {hover: false});
-        hide_card_img($link);
+        $link.data('hover', false);
+        hide_popup($link);
     });
 };
 
 
-//main
+//init
 try{
     after_loaded_jquery(jQuery);
 }catch(e){
