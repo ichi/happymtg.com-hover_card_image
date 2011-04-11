77a81f31316d17caf656fcbcba34a83d242c1762
ichi <ichi.ttht.1@gmail.com>
2011-04-11 19:42:09 +0900

    initial commit

---
+++ happymtg.com-hover_card_image.js
@@ -0,0 +1,119 @@
+// ==UserScript==
+// @name           happymtg.com hover_card_image
+// @namespace      happymtg_com_hover_card_image
+// @version        0.1.0
+// @include        http://www.happymtg.com/*
+// @require        https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js
+// @require        https://github.com/ichi/greasemonkey_console/raw/master/console.js
+// ==/UserScript==
+
+
+var $body = $('body');
+var $card_links = $('span.cardLink > a');
+var datas = {};
+
+var get_data = function($link, name){
+    var href = $link.attr('href');
+    var data = datas[href];
+    if(!data) return false;
+    if(name){
+        return data[name] || false;
+    }
+    return data;
+}
+var set_data = function($link, hash){
+    var href = $link.attr('href');
+    datas[href] = datas[href] || {};
+    $.extend(datas[href], hash);
+}
+
+var show_card_img = function($link){
+    var data = get_data($link);
+    if(!data) return;
+    if(!data.hover) return;
+    var $img = data.card_img;
+    var $text = data.card_text;
+    $img.show();
+    $text.show();
+};
+
+var hide_card_img = function($link){
+    var data = get_data($link);
+    if(!data) return;
+    var $img = data.card_img;
+    var $text = data.card_text;
+    $img.hide();
+    $text.hide();
+};
+
+var on_success = function(data, status, xhr){
+    var $link = $(this);
+    var offset = $link.offset();
+    var $data = $(data);
+    var width = $link.width(),
+    height = $link.height();
+    var $img = $data.find('img.jpnItem')
+        .css({
+            position: 'absolute'
+            , display: 'none'
+            , top: offset.top + height + 5
+            , left: offset.left + width / 2
+        });
+    var $card_detail = $data.find('#carddetail');
+    var text_ja = $card_detail.find('tr.jpnItem:eq(1) > td').text();
+    var text_en = $card_detail.find('tr.engItem:eq(1) > td').text();
+    var $text = $('<div />')
+        .css({
+            position: 'absolute'
+            , display: 'none'
+            , top: offset.top + height + 5
+            , left: offset.left + width / 2 + $img[0].width + 10
+            , padding: '3px'
+            , background: '#999900'
+            , border: '1px solid #cccccc'
+        });
+    var $hr = $('<hr />')
+        .css({
+            height: 1
+            , margin: '3px 0'
+            , border: '1px solid #000000'
+        });
+    $text
+        .append('<div>' + text_ja + '</div>')
+        .append($hr)
+        .append('<div>' + text_en + '</div>')
+    $body
+        .append($img)
+        .append($text);
+    
+    set_data($link, {
+        card_img: $img
+        , card_text: $text
+    });
+    show_card_img($link);
+};
+
+$card_links
+    .live('mouseenter', function(ev){
+        var $link = $(this);
+        set_data($link, {hover: true});
+        var data = get_data($link);
+        if(data.card_img){
+            show_card_img($link);
+        }else{
+            var href = $link.attr('href');
+            $.ajax({
+                url: href
+                , context: $link[0]
+                , type: 'GET'
+                , success: on_success
+            });
+        }
+        set_data($link, {hover: true});
+    })
+    .live('mouseleave', function(ev){
+        var $link = $(this);
+        set_data($link, {hover: false});
+        hide_card_img($link);
+    });
+
